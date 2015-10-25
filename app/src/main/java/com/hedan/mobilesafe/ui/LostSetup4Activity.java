1da4046e9dfdb455e5672b8ca3798a027f5560f9
hedan806 <420978249@qq.com>
2015-10-25 23:58:20 +0800

    Work to Home

--- app/src/main/java/com/hedan/mobilesafe/ui/LostSetup4Activity.java
+++ app/src/main/java/com/hedan/mobilesafe/ui/LostSetup4Activity.java
@@ -1,9 +1,13 @@
 package com.hedan.mobilesafe.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.hedan.mobilesafe.R;
@@ -17,11 +21,39 @@ public class LostSetup4Activity extends Activity{
     private float y1 = 0;
     private float y2 = 0;
 
+    private static final String TAG = "LostSetup4Activity";
+
+    private CheckBox cb_open;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lost_setup_4);
+
+        cb_open = (CheckBox) this.findViewById(R.id.cb_lost_setup_4_opensafe);
+        sp = this.getSharedPreferences("config", Context.MODE_APPEND);
+
+        boolean is_open = sp.getBoolean("open_lost",false);
+        if(is_open){
+            cb_open.setText("保护未开启");
+            cb_open.setChecked(false);
+        }else{
+            cb_open.setText("保护已开启");
+            cb_open.setChecked(true);
+        }
+
+        cb_open.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+
+                }else{
+
+                }
+            }
+        });
+
     }
 
     @Override
